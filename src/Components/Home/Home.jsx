e4082d9bd3287a24d55ff322f361c56cc824e7dd
Indo Krish <55687559+Indok93@users.noreply.github.com>
2024-03-13 16:47:23 +0100

    upd

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,19 +1,7 @@
-import { useEffect, useState } from "react";
+
 import { Header } from "../Partials/Header/Header"
 
 export const Home = () => {
-  const [state, setState] = useState({
-    loading: false,
-    posts: [],
-    error: ''
-  })
-
-  useEffect(() => {
-    const wordPressSiteUrl = '';
-    setState({loading: true});
-
-  }, []); // Empty array means this effect only runs once after the initial render
-
 
   return (
     <div>
